49fc4e78fa2a8bebc6a1960f76e48fbc8e27c1fd
marvelmikel <49599133+marvelmikel@users.noreply.github.com>
2022-05-24 18:27:40 +0100

    notifi

--- resources/views/layouts/individual.blade.php
+++ resources/views/layouts/individual.blade.php
@@ -273,9 +273,9 @@
 								</div>
 							</li>
 							<li class="nav-item dropdown dropdown-large">
-								<a class="nav-link dropdown-toggle dropdown-toggle-nocaret position-relative" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"> <span class="alert-count">7</span>
+								<!--<a class="nav-link dropdown-toggle dropdown-toggle-nocaret position-relative" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"> <span class="alert-count">7</span>
 									<i class='bx bx-bell'></i>
-								</a>
+								</a>-->
 								<div class="dropdown-menu dropdown-menu-end">
 									<a href="javascript:;">
 										<div class="msg-header">
