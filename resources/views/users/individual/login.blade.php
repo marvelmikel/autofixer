ae7a49a76c14756de4a36f8eedf05f6292cdb246
marvelmikel <49599133+marvelmikel@users.noreply.github.com>
2022-05-24 15:41:52 +0100

    forget

--- resources/views/users/individual/login.blade.php
+++ resources/views/users/individual/login.blade.php
@@ -23,7 +23,7 @@
 </head>
 <body>
 
-    <div id="preloader" class="preloader">
+    <div id="preloader"  class="preloader">
         <div class='inner'>
             <div class='line1'></div>
             <div class='line2'></div>
